22906ce5e089aca27948238465bbf46c12141cd2
sie9 <peducris@gmail.com>
2018-05-02 20:45:13 +0100

    chat medio

--- proyecto/front/src/components/MainChat.vue
+++ proyecto/front/src/components/MainChat.vue
@@ -2,7 +2,7 @@
   <div class="container">
       <h1>Titulo chat</h1>
       <languageChoice></languageChoice>
-      <p>{{msg}}</p>
+      <p v-for="mensaje in mensajes">{{mensaje.text}}</p>
       <inputComponent></inputComponent>
   </div>
 </template>
@@ -11,19 +11,34 @@
 
 import inputComponent from './inputComponent'
 import languageChoice from './languageChoice'
+import firebase from 'firebase'
 
 export default {
   name: "MainChat",
   data() {
     return {
-      msg: "Welcome to Your Vue.js App"
+      msg: "Welcome to Your Vue.js App",
+      mensajes:[]
     };
+  },
+  created(){
+      firebase.database().ref('Mensajes').on('child_added', (data) => {
+                
+                
+                this.mensajes.push(data.val())
+                console.log(data.val())
+                /*let objText = {
+                    text: data.val().mensaje,
+                    language: otherlanguage
+                }*/
+            });
   },
   components: {
     inputComponent,
     languageChoice,
   }
 };
+    
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
