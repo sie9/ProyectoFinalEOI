8c0215c80805f9cce8f45874c764dba0da465bd4
Pedro <antonioaren@hotmail.com>
2018-05-02 20:31:52 +0100

    pequeño cambio

--- proyecto/back/api_private/controller.js
+++ proyecto/back/api_private/controller.js
@@ -7,8 +7,7 @@ function probarGet(req,res) {
     return res.json("Probando get");
 }
 
-//Para el post
 function msgReceived (req,res) {
     let objectReceivedToTraslate = req.body
-    return res.json("Recibido para traducir "+ objectReceivedToTraslate.msg);
+    return res.json("Recibido para traducir: "+ objectReceivedToTraslate.msg);
 }
