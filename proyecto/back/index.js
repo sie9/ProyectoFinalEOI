142b3473274577e93c5e5aca5011cbe9faa9067e
Pedro <antonioaren@hotmail.com>
2018-05-02 19:38:10 +0100

    Añadida division del código, dejar preparado routers

--- proyecto/back/index.js
+++ proyecto/back/index.js
@@ -1,17 +1,27 @@
+
+
 const express = require("express");
 const app = express();
 
-//TO-DO: Cuidado con el cors. Permite cualquier petición.
+//TO-DO: Cuidado con el cors. Permite cualquier petición.Configurar.
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+//Obligatorio para el entendimiento de 
+app.use(express.json());
+
+app.route('/').get((req,res,next)=>{
+    res.json("");
+});
+
+app.route('/').post(function(req, res, next) {    
+    let objetReceived = req.body;
+    res.json(objetReceived.msg);
+});
 
 
-const translateRouter = require('./google-translate');
-app.use('/api/translate', translateRouter);
+/* const translateRouter = require('./api/google-translate');
+app.use('/api/translate', translateRouter); */
 
 app.listen(8500, err => {
     console.log("Servidor listo en el puerto 8500");
