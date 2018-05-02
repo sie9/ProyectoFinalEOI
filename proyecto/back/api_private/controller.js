8052629a5e9929339c19d456d6eda38893d5946e
Pedro <antonioaren@hotmail.com>
2018-05-02 19:59:22 +0100

    Metodo mal escrito, cambio en controller

--- proyecto/back/api_private/controller.js
+++ proyecto/back/api_private/controller.js
@@ -1,15 +1,14 @@
 
 
-module.exports.probarGet=probarGet;
-module.exports.objectReceivedToTraslate=objectReceivedToTraslate;
+module.exports.probarGet= probarGet;
+module.exports.msgReceived=msgReceived;
 
 function probarGet(req,res) {
     return res.json("Probando get");
 }
 
-
 //Para el post
 function msgReceived (req,res) {
     let objectReceivedToTraslate = req.body
-    return res.json("Recibido para traducir");
+    return res.json("Recibido para traducir "+ objectReceivedToTraslate.msg);
 }
--- proyecto/back/api_private/index.js
+++ proyecto/back/api_private/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const controller = require('./controller');
 
 router.get('/',controller.probarGet);
-router.post('/',controller.objectReceivedToTraslate);
+router.post('/',controller.msgReceived);
 
 module.exports = router;
