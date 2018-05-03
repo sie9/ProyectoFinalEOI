e835a3b9976e5a474e9a030350160437be519ab2
Pedro <antonioaren@hotmail.com>
2018-05-03 20:06:28 +0100

    Marca de back. Save it

--- proyecto/back/api_private/controller.js
+++ proyecto/back/api_private/controller.js
@@ -15,12 +15,14 @@ function msgReceived (req,res) {
     let traduccion = {};
     axios.post('https://translation.googleapis.com/language/translate/v2?key=AIzaSyDypMznEtSRccdQG5PwbVRdm_fRLhwvQUQ',{
         target : toTraslate.traslate,
-	    q : toTraslate.msg ,
-	    source : toTraslate.from
+	    q : toTraslate.msg 
     })
     .then((response) => {
-        traduccion=_.head(response.data.data.translations).translatedText;        
-        return res.json(traduccion);               
+        traduccion=_.head(response.data.data.translations).translatedText;
+        let txt = {
+            msgTrslated : traduccion
+        }       
+        return res.status(200).json(txt);               
     })
     .catch(err => {
         console.log(err);
