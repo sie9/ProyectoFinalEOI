56f39df796514097cd86094daeb4d6206bfe69cf
Ignacio Suarez GS <ignaciosuarezgs@gmail.com>
2018-05-04 18:49:04 +0100

    nacho cambios

--- proyecto/front/src/components/MainChat.vue
+++ proyecto/front/src/components/MainChat.vue
@@ -2,7 +2,7 @@
   <div class="container">
       <h1>Titulo chat</h1>
       <languageChoice></languageChoice>
-      <PostUser v-for="mensaje in mensajes" :conver="mensaje"/>
+      <PostUser v-for="mensaje in mensajes" :conver="mensaje" :key="mensaje"/>
       <inputComponent></inputComponent>
   </div>
 </template>
@@ -12,7 +12,7 @@ import inputComponent from "./inputComponent";
 import languageChoice from "./languageChoice";
 import firebase from "firebase";
 import PostUser from "./PostUser";
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   name: "MainChat",
@@ -22,7 +22,7 @@ export default {
       mensajes: []
     };
   },
-  created(){
+  created() {
     /* firebase.database().ref('Mensajes').remove(); */
 
     //Esta consulta solo hace falta hacerla una vez.... si veis la consola solo
@@ -30,45 +30,49 @@ export default {
 
     //si estais intentando traducir no se para que le pasais ese json de target y tal....
     // tendria mas sentido hacer un post para traducir no un get :S
-    
-    axios.get('https://translation.googleapis.com/language/translate/v2/languages?key=AIzaSyDypMznEtSRccdQG5PwbVRdm_fRLhwvQUQ',{
-          target :'en',
-	        q :'¿Como te llamas?',
-	        source:'es'
-    }).then((response) => {
-        console.log("idiomas",response.data.data);             
-    }).catch(err => console.log(err))
 
+    axios
+      .get(
+        "https://translation.googleapis.com/language/translate/v2/languages?key=AIzaSyDypMznEtSRccdQG5PwbVRdm_fRLhwvQUQ",
+        {
+          target: "en",
+          q: "¿Como te llamas?",
+          source: "es"
+        }
+      )
+      .then(response => {
+        console.log("idiomas", response.data.data);
+      })
+      .catch(err => console.log(err));
 
-    firebase.database().ref('Mensajes').on('child_added', (data) => {
-        console.log("val", data.val().text)
-        axios.post('https://translation.googleapis.com/language/translate/v2/?key=AIzaSyDypMznEtSRccdQG5PwbVRdm_fRLhwvQUQ',{
-          target :'en',
-	        q : data.val().text,
-	        source:'es'
-        }).then((response) => {
-          console.log("traduccion",JSON.stringify(response.data)); 
-          this.mensajes.push(response.data.translations[0].translatedText);
-                      
-        }).catch(err => console.log(err))
-
-        
-    })
+    firebase
+      .database()
+      .ref("Mensajes")
+      .on("child_added", data => {
+        console.log("val", data.val().text);
+        axios
+          .post(
+            "https://translation.googleapis.com/language/translate/v2/?key=AIzaSyDypMznEtSRccdQG5PwbVRdm_fRLhwvQUQ",
+            {
+              target: "en",
+              q: data.val().text,
+              source: "es"
+            }
+          )
+          .then(response => {
+            console.log("traduccion", JSON.stringify(response.data));
+            this.mensajes.push(response.data.translations[0].translatedText);
+          })
+          .catch(err => console.log(err));
+      });
   },
-                
-                
-                
-                
-                
-                
-                /* console.log(data.val()) */
-                /*let objText = {
+
+  /* console.log(data.val()) */
+  /*let objText = {
                     text: data.val().mensaje,
                     language: otherlanguage
                 }*/
-  
-      
-  
+
   components: {
     PostUser,
     inputComponent,
