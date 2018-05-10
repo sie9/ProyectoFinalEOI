0a0d9a4893adb8d768e5dfe6498f1befc87b4884
sie9 <peducris@gmail.com>
2018-05-10 22:49:42 +0100

    modal contacto y funcionalidad

--- proyecto/front/src/components/navigator.vue
+++ proyecto/front/src/components/navigator.vue
@@ -9,11 +9,11 @@
       <ul id="nav" class="right hide-on-med-and-down">
         <li><a href="#">Yeah</a></li>
         <li><div @click="show()">Add Users</div></li>
-        <li><a href="#" >Contact</a></li>
+        <li><div @click="show3()" >Contact</div></li>
       </ul>
     
     </nav>
-    <!-- The Modal -->
+    <!-- The Modal enviar email -->
     <div id="myModal" class="modal">
 
       <!-- Modal content -->
@@ -42,11 +42,40 @@
     </div>
 
     <!-- The Modal exito email -->
-    <div id="myModal2" class="modal">
+    <div id="myModal2" class="modal2">
       <!-- Modal content -->
-      <div class="modal-content">
+      <div class="modal-content2">
         <span class="close" @click="hide2">&times;</span>
         <h3>Se ha enviado tu email!!</h3>
+      </div>
+    </div>
+
+    <!-- The Modal contacto -->
+    <div id="myModal3" class="modal">
+
+      <!-- Modal content -->
+      <div class="modal-content">
+        <span class="close" @click="hide3">&times;</span>
+        <h3>¿Como podemos ayudarte?</h3>
+                    
+        <div class="row">
+          <div class="input-field col s12">
+            <input id="email" type="email" class="validate"  v-model="contacto.email" >
+            <label for="email" >Email</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <input id="asunto" type="text" class="validate" v-model="contacto.asunto" >
+            <label for="asunto" >Asunto</label>
+          </div>
+        </div>
+       
+        <div class="row">
+            <div class="card-panel teal lighten-3 flow-text  offset-s2 col s4" style= "background-color: #000;">Cancelar</div>
+            <div class="card-panel teal lighten-1 flow-text col s5 " style= "background-color: #111;" @click="onSubmit2()">Enviar</div>
+          
+        </div>
       </div>
     </div>
           
@@ -74,7 +103,11 @@ export default {
   data() {
     return {
       email: "",
-      emails: []
+      emails: [],
+      contacto: {
+        email:"",
+        asunto:""
+      }
       /* lang:"" */
     };
   },
@@ -91,11 +124,29 @@ export default {
         );
         $("#email").val("");
         this.emails.push(this.email);
-        this.sendMail();        
+        this.sendMail();
+      }
+    },
+    onSubmit2() {
+      if (this.contacto.email.includes("@")) {
+        this.sendMail2();
       }
     },
     show() {
       var modal = document.getElementById("myModal");
+      modal.style.display = "block";
+    },
+    show2() {
+      var modal = document.getElementById("myModal2");
+      modal.style.display = "block";
+
+      setTimeout(() => {
+        this.hide2();
+        this.hide();
+      }, div000);
+    },
+    show3() {
+      var modal = document.getElementById("myModal3");
       modal.style.display = "block";
     },
     hide() {
@@ -104,9 +155,14 @@ export default {
       this.emails = [];
       this.email = "";
     },
+
     hide2() {
       var modal = document.getElementById("myModal2");
-      modal.style.display = "none";      
+      modal.style.display = "none";
+    },
+    hide3() {
+      var modal = document.getElementById("myModal3");
+      modal.style.display = "none";
     },
 
     sendMail() {
@@ -118,9 +174,8 @@ export default {
         template_id: "chatty",
         user_id: "user_DeVh4AytZ9lQqdCTn9ODu",
         template_params: {
-          email : this.email,
-          sala : "http://localhost:8080/#/"+res   
-          
+          email: this.email,
+          sala: "http://localhost:8080/#/" + res
         }
       };
 
@@ -129,8 +184,34 @@ export default {
         data: JSON.stringify(data),
         contentType: "application/json"
       })
-        .done(function() {
-          alert("Your mail is sent!");
+        .done(() => {
+          this.show2();
+        })
+        .fail(function(error) {
+          console.log("Oops... " + JSON.stringify(error));
+        });
+    },
+    sendMail2() {
+      var route = this.$route.path;
+      var res = route.substring(1, route.length);
+
+      let data = {
+        service_id: "pry.chtty@gmail.com",
+        template_id: "prueba",
+        user_id: "user_DeVh4AytZ9lQqdCTn9ODu",
+        template_params: { 
+          contacto: this.contacto.email,
+          asunto: this.contacto.asunto
+        }
+      };
+
+      $.ajax("https://api.emailjs.com/api/v1.0/email/send", {
+        type: "POST",
+        data: JSON.stringify(data),
+        contentType: "application/json"
+      })
+        .done(() => {
+          this.show2();
         })
         .fail(function(error) {
           console.log("Oops... " + JSON.stringify(error));
@@ -144,37 +225,36 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 
 <style scoped>
-
-nav{
-    background:rgb(169, 170, 173);
-    display:flex;
-    flex-wrap: nowrap;
+nav {
+  background: rgb(169, 170, 173);
+  display: flex;
+  flex-wrap: nowrap;
 }
 
-.brand-logo{
-    margin-left:20px;
-    position:initial;
-    width: 15%;
-    text-align:left;
+.brand-logo {
+  margin-left: 20px;
+  position: initial;
+  width: 15%;
+  text-align: left;
 }
 
-.language-choice{
-  margin-top:10px;
+.language-choice {
+  margin-top: 10px;
 }
 
-.select-wrapper input.select-dropdown{
-  border-bottom:none;
+.select-wrapper input.select-dropdown {
+  border-bottom: none;
 }
 
-img{
-  width:40%;
+img {
+  width: 40%;
   filter: grayscale(100%);
   transition: filter 0.3s ease-out;
   padding-top: 5px;
 }
 
-img:hover{
-  transition:filter .3s ease-in;
+img:hover {
+  transition: filter 0.3s ease-in;
   filter: grayscale(0%);
 }
 
@@ -197,6 +277,23 @@ share-link {
   border: 1px solid #888;
   width: 100%;
   height: 100%;
+}
+
+/* The Modal (background) */
+.modal2 {
+  display: none; /* Hidden by default */
+  z-index: auto;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
+}
+
+/* Modal Content */
+.modal-content2 {
+  background-color: #fefefe;
+  margin: auto;
+  border: 1px solid #888;
+  width: 100px;
+  height: auto;
 }
 
 /* The Close Button */
